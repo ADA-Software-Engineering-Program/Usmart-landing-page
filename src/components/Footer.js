62db4ffb71a2d6e319aa77ef37d569465bd88640
phyya <phaboolous11@gmail.com>
2023-02-24 19:31:00 +0100

    feat: added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import logo from '../assets/logo-transparent.png';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<section className="footer">
+			<img src={logo} alt="u-smart logo" className="img-logo" />
+			<h3>&copy; Copyrights 2023 ADAPROJECT All rights reserved.</h3>
+		</section>
+	);
 };
 
 export default Footer;
