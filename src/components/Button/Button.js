55a18de8d33b7e98c1f08e286b3f333bebaa2a3c
phyya <phaboolous11@gmail.com>
2023-03-15 19:53:13 +0100

    fix: new design

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,7 @@ export const PrimaryButton = ({ text }) => {
 	return (
 		<button className="primary-btn btn">
 			<a
-				href="https://firebasestorage.googleapis.com/v0/b/usmart-baff2.appspot.com/o/usmart-apk.apk?alt=media&token=15c4eddf-cd0b-48b1-b333-0c8ffb1fc2af"
+				href="https://firebasestorage.googleapis.com/v0/b/usmart-baff2.appspot.com/o/usmart-apkv2.apk?alt=media&token=abbe98f4-34f0-44b3-a7fa-31f01365eefd"
 				download
 			>
 				{text}
@@ -24,7 +24,7 @@ export const OtherButton = ({ text }) => {
 	return (
 		<button className="other-btn btn">
 			<a
-				href="https://firebasestorage.googleapis.com/v0/b/usmart-baff2.appspot.com/o/usmart-apk.apk?alt=media&token=15c4eddf-cd0b-48b1-b333-0c8ffb1fc2af"
+				href="https://firebasestorage.googleapis.com/v0/b/usmart-baff2.appspot.com/o/usmart-apkv2.apk?alt=media&token=abbe98f4-34f0-44b3-a7fa-31f01365eefd"
 				download
 			>
 				{text}
